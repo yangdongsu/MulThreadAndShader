befdebe1bcaf210a52f226f9a60b8bab86d442fb
sdy <420099788@qq.com>
2019-04-23 14:05:03 +0800

    提交自动打包构建

---
+++ TransparentShader/Assets/Editor/APKBuild.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class APKBuild : Editor
+{
+    [MenuItem("Tool/APKBuild")]
+    public static void Build()
+    {
+        BuildTarget buildTarget = BuildTarget.Android;
+        // 切换到 Android 平台        
+        EditorUserBuildSettings.SwitchActiveBuildTarget(buildTarget);
+        // keystore 路径, G:\keystore\one.keystore        
+        PlayerSettings.Android.keystoreName = "G:\\keystore\\one.keystore";
+        // one.keystore 密码        
+        PlayerSettings.Android.keystorePass = "123456";
+        // one.keystore 别名        
+        PlayerSettings.Android.keyaliasName = "bieming1";
+        // 别名密码        
+        PlayerSettings.Android.keyaliasPass = "123456";
+        List<string> levels = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            // 获取有效的 Scene            
+            levels.Add(scene.path);
+        }
+        // 打包出 APK 名        
+        string apkName = string.Format("./{0}.apk", "Test");
+        // 执行打包        
+        UnityEditor.Build.Reporting.BuildReport br = BuildPipeline.BuildPlayer(levels.ToArray(), apkName, buildTarget, BuildOptions.None);
+        AssetDatabase.Refresh();
+    }
+}
