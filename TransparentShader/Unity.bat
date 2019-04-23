e2e389b6f2bf93436f3003fc02b6286c8c64b265
苏东阳 <420099788@qq.com>
2019-04-23 17:46:02 +0800

    tijiao

--- TransparentShader/Unity.bat
+++ TransparentShader/Unity.bat
@@ -6,6 +6,6 @@ set UNITY_LOG_PATH=%cd%\unity_log.txt
 
 echo ∆Ù∂Ø Unity.exe «Î…‘∫Û...
 
-%1 -quit -batchmode -logFile %UNITY_LOG_PATH% -projectPath %2 -executeMethod APKBuild.Build
+%UNITY_PATH% -quit -batchmode -logFile %UNITY_LOG_PATH% -projectPath %UNITY_PROJECT_PATH% -executeMethod APKBuild.Build
 
 pause
