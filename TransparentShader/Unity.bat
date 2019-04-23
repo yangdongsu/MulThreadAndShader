6caf4f58b32558098f1b15b82de254f4483ff199
苏东阳 <420099788@qq.com>
2019-04-23 14:28:17 +0800

    更新unity。bat

--- TransparentShader/Unity.bat
+++ TransparentShader/Unity.bat
@@ -2,7 +2,7 @@
 
 echo 正在生成APK文件... 
  
-C:\Program Files\Unity\Hub\Editor\2018.3.9f1\Editor\Unity.exe -projectPath E:\GitItem\MulThreadAndShader\TransparentShader -quit -batchmode -executeMethod APKBuild.Build -logFile build.log
+REM C:\Program Files\Unity\Hub\Editor\2018.3.9f1\Editor\Unity.exe -projectPath E:\GitItem\MulThreadAndShader\TransparentShader -quit -batchmode -executeMethod APKBuild.Build -logFile build.log
 
 %1 -projectPath %2 -quit -batchmode -executeMethod APKBuild.Build -logFile build.log
 
