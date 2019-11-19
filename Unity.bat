4dd0caf3149439987968913c980b939d2e171eb0
苏东阳 <sudongyang@rnd5.iggcn.com>
2019-11-19 11:17:05 +0800

    提交未提交

--- Unity.bat
+++ Unity.bat
@@ -4,8 +4,8 @@ set UNITY_PATH="C:\Program Files\Unity\Hub\Editor\2018.3.9f1\Editor\Unity.exe"
 set UNITY_PROJECT_PATH=TransparentShader
 set UNITY_LOG_PATH= unity_log.txt
 
-echo ∆Ù∂Ø Unity.exe «Î…‘∫Û...
+echo ?? Unity.exe ???...
 
-%UNITY_PATH% -quit -batchmode -logFile %UNITY_LOG_PATH% -projectPath %UNITY_PROJECT_PATH%  -executeMethod APKBuild.Build
+%UNITY_PATH% -quit -batchmode  -logFile %UNITY_LOG_PATH% -projectPath %UNITY_PROJECT_PATH%  -executeMethod APKBuild.Build
 
 pause
